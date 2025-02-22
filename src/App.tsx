e87d79b3f107d4040ab8fd0f9b550d98fe424763
harunar21 <harun@rekosistem.com>
2025-02-22 14:32:44 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Text, VStack } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <VStack>
+      <Text>Hello World!</Text>
+    </VStack>
+  );
+};
+
+export default App;
