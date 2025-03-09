3f9bdc4bec8bfe4fb4f67f7f78ae4658794dfc0b
harunar21 <harun@rekosistem.com>
2025-03-09 09:08:04 +0700

    - Feat: Add react-hook-form, yup, and yup resolver - Feat: Add create and join hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </StrictMode>,
 );
