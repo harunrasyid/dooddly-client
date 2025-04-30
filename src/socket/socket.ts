14cf6c4b2b4b12120ee8eedd176087eea9afbae1
harunar21 <harun@rekosistem.com>
2025-04-30 08:46:18 +0700

    - Feat: Add undo redo and clear canvas function

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = "https://dooddly-server-fe6f5300467a.herokuapp.com";
+const SOCKET_URL = "https://dooddly-server.onrender.com";
 
 export const socket: Socket = io(SOCKET_URL, {
   transports: ["websocket"],
